06bfe69668fdd2259bdaafb8f634befe12f14a3c
Muhammad Iskandar Dzulqornain <midzulqornain@gmail.com>
2020-01-12 14:37:09 +0700

    init migration tables

---
+++ migration/001_init_tables.sql
@@ -0,0 +1,53 @@
+CREATE TABLE `Command` (
+  `id` char(36) NOT NULL,
+  `teamID` char(36) DEFAULT NULL,
+  `name` char(20) NOT NULL,
+  `description` text,
+  `example` text,
+  `completeDescription` text,
+  `created`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `createdBy` char(36) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE (`name`),
+  UNIQUE `teamIDName` (`teamID`, `name`)
+);
+
+CREATE TABLE `Option` (
+  `id` char(36) NOT NULL,
+  `commandID` char(36) DEFAULT NULL,
+  `name` char(20) NOT NULL,
+  `value` text,
+  `shortName` char(10),
+  `description` text,
+  `isSingleOption` boolean DEFAULT false,
+  `isMandatory` boolean DEFAULT false,
+  `isMultipleValue` boolean DEFAULT false,
+  `isDynamic` boolean DEFAULT false,
+  `isEncrypted` boolean DEFAULT false,
+  `isCustom` boolean DEFAULT false,
+  `example` text,
+  `optionAlias` char(20) DEFAULT NULL,
+  `created`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `createdBy` char(36) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE `commandIDName` (`commandID`,`name`),
+  UNIQUE `commandIDShortName` (`commandID`,`shortName`)
+);
+
+CREATE TABLE `Slackbot` (
+  `id` char(36) NOT NULL,
+  `name` text,
+  `created`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `createdBy` char(36) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Team` (
+  `id` char(36) NOT NULL,
+  `name` text,
+  `domain` text,
+  `emailDomain` text,
+  `created`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `createdBy` char(36) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
