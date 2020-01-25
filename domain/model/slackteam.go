c44c7e18a373f2d35901e762811b699b2662613c
Muhammad Iskandar Dzulqornain <midzulqornain@gmail.com>
2020-01-25 21:15:25 +0700

    add slackID and remove unused fields

--- domain/model/slackteam.go
+++ domain/model/slackteam.go
@@ -3,16 +3,15 @@ package model
 import (
 	"time"
 
-	"github.com/nlopes/slack"
 	uuid "github.com/satori/go.uuid"
 )
 
 type SlackTeamModel struct {
 	ID          uuid.UUID `json:"id" db:"id"`
+	SlackID     string    `json:"string" db:"string"`
 	Name        string    `json:"name" db:"name"`
 	Domain      string    `json:"domain" db:"domain"`
 	EmailDomain string    `json:"emailDomain" db:"emailDomain"`
 	Created     time.Time `json:"created" db:"created"`
 	CreatedBy   string    `json:"createdBy" db:"createdBy"`
-	Team        slack.TeamInfo
 }
