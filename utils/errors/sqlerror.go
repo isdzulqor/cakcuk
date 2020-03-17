6b8e80802e8bbb404d0c98db8ca53bd115acd4ea
Muhammad Iskandar Dzulqornain <midzulqornain@gmail.com>
2020-03-18 00:21:04 +0700

    update duplicate msg

--- utils/errors/sqlerror.go
+++ utils/errors/sqlerror.go
@@ -10,7 +10,7 @@ func TranslateSQLError(in error) error {
 	if sqlErr, ok := (in).(*mysql.MySQLError); ok {
 		switch sqlErr.Number {
 		case 1062:
-			return fmt.Errorf("It already exists in a database. %s", sqlErr.Message)
+			return fmt.Errorf("It already exists in a database.")
 		}
 	}
 	return in
