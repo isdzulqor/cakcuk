95c1e0218455ba97803c5bcb9a0463000980d607
Muhammad Iskandar Dzulqornain <midzulqornain@gmail.com>
2020-06-15 00:00:08 +0700

    add Cakcuk readiness on test integration CI

--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -14,16 +17,38 @@ func main() {
 			port = tempPort
 		}
 	}
-	url := "http://localhost:" + port + "/api/health"
-	req, err := http.NewRequest("GET", url, nil)
+	healthURL := "http://localhost:" + port + "/api/health"
+
+	givenRetry := flag.Int("retry", 1, "Set retry number to check healthcheck. Program will sleep for 15 seconds before executing the next retry.")
+	flag.Parse()
+
+	var err error
+	for *givenRetry != 0 {
+		if err = checkHealth(healthURL); err == nil {
+			break
+		}
+		*givenRetry--
+		if *givenRetry > 0 {
+			log.Printf("Program will sleep for 15 seconds. %d retries left...\n", *givenRetry)
+			time.Sleep(15 * time.Second)
+		}
+	}
 	if err != nil {
 		os.Exit(1)
+	}
+	log.Println("Cakcuk is healthy...")
+}
+
+func checkHealth(healthURL string) error {
+	req, err := http.NewRequest("GET", healthURL, nil)
+	if err != nil {
+		return err
 	}
 	req.Header.Add("x-cakcuk-secret-key", os.Getenv("SECRET_KEY"))
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		os.Exit(1)
-		return
+		return err
 	}
 	defer res.Body.Close()
+	return nil
 }
