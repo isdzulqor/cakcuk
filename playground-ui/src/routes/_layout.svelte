6bd5ad438d4c87176f0e950de1c0c46498c4cbcf
Muhammad Iskandar Dzulqornain <midzulqornain@gmail.com>
2020-07-03 13:56:32 +0700

    remove comments

--- playground-ui/src/routes/_layout.svelte
+++ playground-ui/src/routes/_layout.svelte
@@ -1,19 +1,10 @@
 <script>
-	// import Nav from '../components/Nav.svelte';
 	import Navbar from '../components/Navbar.svelte';
 
 	export let segment;
 </script>
 
 <style>
-	/* main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	} */
 </style>
 
 <Navbar {segment}/>
